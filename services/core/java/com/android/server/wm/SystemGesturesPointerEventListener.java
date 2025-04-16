3b0ae16b7bf4ed3eb06a9ca3b8f82d5221333249
murikbmx <murikbmx@gmail.com>
2025-04-16 18:19:49 +0500

    services: Implement scroll gesture boosting

--- services/core/java/com/android/server/wm/SystemGesturesPointerEventListener.java
+++ services/core/java/com/android/server/wm/SystemGesturesPointerEventListener.java
@@ -91,6 +91,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
     private boolean mMouseHoveringAtRight;
     private boolean mMouseHoveringAtBottom;
     private long mLastFlingTime;
+    private boolean mScrollFired;
 
     SystemGesturesPointerEventListener(Context context, Handler handler, Callbacks callbacks) {
         mContext = checkNull("context", context);
@@ -184,6 +185,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             case MotionEvent.ACTION_DOWN:
                 mSwipeFireable = true;
                 mDebugFireable = true;
+                mScrollFired = false;
                 mDownPointers = 0;
                 captureDown(event, 0);
                 if (mMouseHoveringAtLeft) {
@@ -290,6 +292,7 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             case MotionEvent.ACTION_CANCEL:
                 mSwipeFireable = false;
                 mDebugFireable = false;
+                mScrollFired = false;
                 mCallbacks.onUpOrCancel();
                 break;
             default:
@@ -449,6 +452,19 @@ class SystemGesturesPointerEventListener implements PointerEventListener {
             mLastFlingTime = now;
             mCallbacks.onFling(duration);
             return true;
+        }
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                   float distanceX, float distanceY) {
+           if (!mScrollFired) {
+                int duration = mOverscroller.getDuration();
+                if (duration > MAX_FLING_TIME_MILLIS) {
+                    duration = MAX_FLING_TIME_MILLIS;
+                }
+               mCallbacks.onScroll(duration);
+               mScrollFired = true;
+           }
+           return true;
         }
     }
 
