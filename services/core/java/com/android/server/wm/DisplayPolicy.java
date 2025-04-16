6324aa89ed18bba38cacc56433b467301121d42f
murikbmx <murikbmx@gmail.com>
2025-04-16 19:11:51 +0500

    Revert "services: Implement scroll gesture boosting"
    This reverts commit 858c6657737ba84278f2733cea54c8580f8309e9.

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -538,14 +538,6 @@ public class DisplayPolicy {
                                 Boost.INTERACTION, duration);
                     }
                 }
-                
-                @Override
-                public void onScroll(int duration) {
-                    if (mService.mPowerManagerInternal != null) {
-                        mService.mPowerManagerInternal.setPowerBoost(
-                                Boost.INTERACTION, duration);
-                    }
-                }
 
                 @Override
                 public void onDebug() {
