603354dc3659f41de32b736520d253e0d69b07f5
murikbmx <murikbmx@gmail.com>
2025-06-10 14:20:33 +0500

    Revert "OnGoingActionProgressController: Store current drawable for icon"
    This reverts commit 6751dd77dd24625e43c8438290765a9aa8c4a6b2.

--- packages/SystemUI/src/com/android/systemui/statusbar/OnGoingActionProgressController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/OnGoingActionProgressController.java
@@ -155,7 +155,6 @@ public class OnGoingActionProgressController implements NotificationListener.Not
         mCurrentProgress = notification.extras.getInt(Notification.EXTRA_PROGRESS, 0);
         IconFetcher.AdaptiveDrawableResult drawable =
                 mIconFetcher.getMonotonicPackageIcon(sbn.getPackageName());
-        mCurrentDrawable = drawable.drawable;
         updateIconImageView(drawable);
         updateViews();
     }
