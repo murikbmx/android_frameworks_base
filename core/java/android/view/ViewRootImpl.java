7b9528143c6bad742f5a6784f79bc0f2e9447b80
murikbmx <murikbmx@gmail.com>
2025-09-25 20:24:59 +0500

    base: Add three-fingers-swipe to screenshot [1/2] The feature is ported from Oppo ColorOS. With the options on, users can swipe down on the screen to have a screenshot. Original smali port by: wuxianlin
    Change-Id: I87ed333f81e1259758ea899bf019061d8ab85fd0
    
    base: Improvements for swipe to screenshot
    
    * Cancel touch events when tap with three fingers
    
    * Dyrex2004: Fix typos (Screnshot -> Screenshot)
    
    Change-Id: I6e1228e4101aa357d1e26ca6d0db1c44428ef367
    
    base: SwipeToScreenshot: Import MIUI implementation
    
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    idoybh (YAAP):
    Improved code all across, remove the usage of a system prop and more...
    also properly add co-authors
    
    Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>
    Co-Authored-By: wuxianlin <wuxianlinwxl@gmail.com>
    
    Change-Id: I1d5938457b7cc4c5ef1fec235d27d8a40526961a
    Co-Authored-By: Tobias Merkel <stylogey@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -8023,6 +8023,11 @@ public final class ViewRootImpl implements ViewParent,
                 // If handwriting is started, toolkit doesn't receive ACTION_UP.
                 mLastClickToolType = event.getToolType(event.getActionIndex());
             }
+            
+            if (event.getPointerCount() == 3 && isSwipeToScreenshotGestureActive()) {
+                 event.setAction(MotionEvent.ACTION_CANCEL);
+                 Log.d("SwipeToScreenShot", "canceling motionEvent because of threeGesture detecting");
+            }
 
             mAttachInfo.mUnbufferedDispatchRequested = false;
             mAttachInfo.mHandlingPointerEvent = true;
@@ -13338,5 +13343,14 @@ public final class ViewRootImpl implements ViewParent,
         } else {
             mInfrequentUpdateCount = 0;
         }
+    }
+    
+    private boolean isSwipeToScreenshotGestureActive() {
+         try {
+             return ActivityManager.getService().isSwipeToScreenshotGestureActive();
+         } catch (RemoteException e) {
+             Log.e("SwipeToScreenshot", "isSwipeToScreenshotGestureActive exception", e);
+             return false;
+         }
     }
 }
